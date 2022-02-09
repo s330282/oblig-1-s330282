c7a7deddc17b72346ea77d20182e6e4e79e575e0
s330282 <91112843+s330282@users.noreply.github.com>
2022-02-09 21:07:48 +0100

    Initial commit

---
+++ src/main/java/com/example/oblig1s330282/Oblig1S330282Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig1s330282;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1S330282Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1S330282Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1s330282/Oblig1S330282ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1s330282;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1S330282ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
